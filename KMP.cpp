8e0b9054397bae97765eba23187cf20772dc28c3
unknown <nazywam@gmail.com>
2016-01-27 23:03:53 +0100

    Fixed yet again

--- KMP.cpp
+++ KMP.cpp
@@ -7,9 +7,9 @@ void kmp(string pattern, string text, int* prefixTable){ //run prefix algorithm 
 	int c = 0;
 	for(int i=0; i<data.size(); i++){
 		while(c > 0 && (data[c] != data[i]) || c == pattern.size()){
-			c = prefixTable[c-1];
+			c = prefixTable[c];
 		}
-		if(data[c] == data[i]){
+		if(data[c] == data[i] && c != i){
 			c++;
 		}
 		prefixTable[i+1] = c;
@@ -17,8 +17,8 @@ void kmp(string pattern, string text, int* prefixTable){ //run prefix algorithm 
 }
 
 int main(){
-	string pattern = "a";
-	string text = "aaaaaa";
+	string pattern = "cat";
+	string text = "tacocattacocat";
 	string data = pattern+'#'+text;
 
 	int* prefixTable = new int[data.size()+1];
@@ -26,9 +26,9 @@ int main(){
 	kmp(pattern, text, prefixTable);
 	
 	//find occurences of pattern in text
-	for(int x=pattern.size()+2; x<data.size(); x++){
+	for(int x=1; x<=data.size(); x++){
 		if(prefixTable[x] == pattern.size()){ 
-			cout << "Found " << pattern << " at " << x-pattern.size()-2 << endl;
+			cout << "Found " << pattern << " at " << x-pattern.size()*2-1 << endl;
 		}
 	}
 }
