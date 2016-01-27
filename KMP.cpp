5e5b8c275e2a8f136995aba73b664d191028e57a
unknown <nazywam@gmail.com>
2016-01-27 22:51:09 +0100

    Fixed

--- KMP.cpp
+++ KMP.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void kmp(string data, int* prefixTable){ //run prefix algorithm on data and put values in prefixTable
+void kmp(string pattern, string text, int* prefixTable){ //run prefix algorithm on data and put values in prefixTable
 	prefixTable[0] = 0;
+	string data = pattern+'#'+text;
 	int c = 0;
-	for(int i=1; i<data.size(); i++){
-		while(c > 0 && data[c] != data[i]){
-			c = prefixTable[c];
+	for(int i=0; i<data.size(); i++){
+		while(c > 0 && (data[c] != data[i]) || c == pattern.size()){
+			c = prefixTable[c-1];
 		}
 		if(data[c] == data[i]){
 			c++;
 		}
-		prefixTable[i] = c;
+		prefixTable[i+1] = c;
 	}
 }
 
 int main(){
-	string pattern = "cat";
-	string text = "tacocattacocat";
+	string pattern = "a";
+	string text = "aaaaaa";
 	string data = pattern+'#'+text;
 
-	int* prefixTable = new int[data.size()];
+	int* prefixTable = new int[data.size()+1];
 
-	kmp(data, prefixTable);
+	kmp(pattern, text, prefixTable);
 	
 	//find occurences of pattern in text
-	for(int x=0; x<data.size(); x++){
+	for(int x=pattern.size()+2; x<data.size(); x++){
 		if(prefixTable[x] == pattern.size()){ 
-			cout << "Found " << pattern << " at " << x << endl;
+			cout << "Found " << pattern << " at " << x-pattern.size()-2 << endl;
 		}
 	}
 }
